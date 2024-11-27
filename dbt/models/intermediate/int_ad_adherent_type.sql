856e25b492dd6cd9e7dbc6f6acd4cf3946e37f32
Agathe Dubly <agathe.dubly.pro@gmail.com>
2024-11-27 13:04:37 +0000

    test V2

--- dbt/models/intermediate/int_ad_adherent_type.sql
+++ dbt/models/intermediate/int_ad_adherent_type.sql
@@ -1,41 +1,16 @@
-SELECT * FROM {{ ref('stg_datasources__entite_gestion') }}
+select *
+from {{ ref("int_all_data_with_clusters") }}
 
+-- est ce que le la colonne id_sespea_entité de gestion est une clé primaire ? yes
+-- c'est bon
+select id_sispea_entite_de_gestion, year, count(*) as nb
+from {{ ref("int_all_data_with_clusters") }}
+group by year, id_sispea_entite_de_gestion
+having nb > 1
 
--- colonnes dont j'ai besoin pour l'analyse 
-SELECT departement, id_sispea_collectivite, communes_adherentes_entite_de_gestion, pop_entite_de_gestion_sans_double_compte
-FROM {{ ref('stg_datasources__entite_gestion') }}
-
-
-
--- ajouter une colonne avec le % de commune adhérant seule sur le total de commune 
-
-SELECT year,
-sum(communes_adherentes_entite_de_gestion) AS qte_communes, 
-FROM {{ ref('stg_datasources__entite_gestion') }}
-Group by year 
-Order by year
-
-SELECT id_sispea_collectivite, year,
-    CASE   
-        WHEN communes_adherentes_entite_de_gestion = 1 THEN 1
-        ELSE 0 
-    END AS commune_solo
-FROM {{ ref('stg_datasources__entite_gestion') }}
-
-
--- Objectif calculer le poids des données manquantes en terme de population représentée => les données sont complétées pour 52% de la population
-WITH NullPricetab AS (
-SELECT id_sispea_entite_de_gestion, 
-pop_entite_de_gestion_sans_double_compte,
-    CASE 
-        WHEN prix_ttc_m3 > 1 THEN 1
-        ELSE 0
-    END AS NullPrice
-FROM {{ ref('stg_datasources__entite_gestion') }}
-)
-
-SELECT NullPrice,
-sum(pop_entite_de_gestion_sans_double_compte)
-FROM NullPricetab
-GROUP BY NullPrice
-
+-- Evolution du nombre de commune solo adhérent à une entité de gestion, il manque la
+-- donnée nombre de commune par année
+select communes_adherentes_entite_de_gestion, year, count(*) as nb
+from {{ ref("int_all_data_with_clusters") }}
+group by year, communes_adherentes_entite_de_gestion
+having communes_adherentes_entite_de_gestion = 1
