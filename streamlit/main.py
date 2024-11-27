0ded586d1681e2c18f81706d3868ed347cf4cf70
bennismehdi91 <bennismehdi91@gmail.com>
2024-11-27 16:17:14 +0100

    add dataframe into streamlit

--- streamlit/main.py
+++ streamlit/main.py
@@ -1,8 +1,17 @@
 import streamlit as st
+from google.cloud import bigquery
+import pandas as pd
 
 st.markdown('Test test')
 
 "test test"
 
 st.header('title')
+
+# Initialize the BigQuery client
+client = bigquery.Client()
+
+# Query BigQuery
+query = "SELECT * FROM `eaupotable-442812.dbt_elewagon.mart_mb_correlation_abonnesprice` LIMIT 10"
+df = client.query(query).to_dataframe()
 
