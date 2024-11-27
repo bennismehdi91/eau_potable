dde262de0f13da4edca114e21c59cd1e04dde5b8
bennismehdi91 <bennismehdi91@gmail.com>
2024-11-27 16:28:09 +0100

    change big query df

--- streamlit/main.py
+++ streamlit/main.py
@@ -15,5 +15,21 @@ client = bigquery.Client.from_service_account_info(credentials)
 
 # Query BigQuery
 query = "SELECT * FROM `eaupotable-442812.dbt_elewagon.mart_mb_correlation_abonnesprice` LIMIT 10"
-df = client.query(query).to_dataframe()
+
+# Run the query
+query_job = client.query(query)
+
+# Get the results
+results = query_job.result()
+
+# Extract column names from the schema
+columns = [field.name for field in results.schema]
+
+# Convert rows to a list of dictionaries
+data = [dict(row.items()) for row in results]
+
+# Create a Pandas DataFrame
+results_df = pd.DataFrame(data, columns=columns)
+
+print(results_df)
 
