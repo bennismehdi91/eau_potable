5c6c68cc1f8919ead4d39f3ab73a5f43bab8b854
EauPotable LeWagon <eaupotablelewagon@gmail.com>
2024-11-28 16:16:11 +0000

    commit - new int tables

--- dbt/models/intermediate/int_mb_analyse_cities.sql
+++ dbt/models/intermediate/int_mb_analyse_cities.sql
@@ -17,6 +17,7 @@ WITH
         FROM source
         WHERE annee = '2015' 
           AND cluster_3_pop_service = '1 - moins de 1000'
+          AND communes_adherentes_entite_de_gestion = 1
     )
 SELECT
     annee,
@@ -24,16 +25,15 @@ SELECT
     AVG(prix_ttc_m3) AS avg_prix_ttc_m3,
     AVG(tx_conformite_microbiologie) AS avg_tx_conformite_microbiologie,
     AVG(tx_conformite_physiochimiques) AS avg_tx_conformite_physiochimiques,
+    AVG(IPL_note) AS IPL_note,
     CASE
         WHEN communes_adherentes_entite_de_gestion = 1 THEN 'solo'
         WHEN communes_adherentes_entite_de_gestion > 1 THEN 'multi'
         ELSE 'NA'
     END AS solo_multi,
-    IPL_note,
-    cluster_3_pop_service
 FROM
     source
 WHERE
     code_insee_commune_adherente IN (SELECT code_insee_commune_adherente FROM check_cte)
 GROUP BY
-    annee, code_insee_commune_adherente, communes_adherentes_entite_de_gestion, IPL_note, cluster_3_pop_service
+    annee, code_insee_commune_adherente, communes_adherentes_entite_de_gestion
