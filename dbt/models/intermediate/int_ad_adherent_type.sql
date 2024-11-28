ab8625ecb5fa51795d4213b682c13ec581fdcc65
Agathe Dubly <agathe.dubly.pro@gmail.com>
2024-11-28 09:04:49 +0000

    Agathe_explo_diverses

--- dbt/models/intermediate/int_ad_adherent_type.sql
+++ dbt/models/intermediate/int_ad_adherent_type.sql
@@ -14,3 +14,21 @@ select communes_adherentes_entite_de_gestion, year, count(*) as nb
 from {{ ref("int_all_data_with_clusters") }}
 group by year, communes_adherentes_entite_de_gestion
 having communes_adherentes_entite_de_gestion = 1
+
+-- nombre collectivité et quantité de communes : il y 1 collectivité avec 645 communes mais 66464 collectivité avec une seule commune
+
+With commune_par_collectivite_adherente AS (
+select communes_adherentes_entite_de_gestion, 
+year, 
+count(*) as nb_collectivite
+from {{ ref("int_all_data_with_clusters") }}
+group by year, communes_adherentes_entite_de_gestion
+)
+
+SELECT communes_adherentes_entite_de_gestion,
+sum(nb_collectivite) as sum_nb,
+FROM commune_par_collectivite_adherente
+Group by communes_adherentes_entite_de_gestion
+ORDER BY communes_adherentes_entite_de_gestion
+
+
