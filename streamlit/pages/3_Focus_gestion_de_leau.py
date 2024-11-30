79bb79e050fb6e425bc7cedac23f5df8895d2ee2
bennismehdi91 <bennismehdi91@gmail.com>
2024-11-30 16:20:42 +0100

    push file without conflict

--- streamlit/pages/3_Focus_gestion_de_leau.py
+++ streamlit/pages/3_Focus_gestion_de_leau.py
@@ -1,44 +1,21 @@
-<<<<<<< HEAD
 import streamlit as st
 import pandas as pd
 from google.cloud import bigquery
 import os
 import plotly.express as px
-=======
-import streamlit as st ## pour récupérer streamlit
-import pandas as pd # always need pandas
-from google.cloud import bigquery # pour se connecter à bigQuery et récupérer les tables préparée
-import os # pour s'autentifier à Google Cloud Plateform
-import plotly.express as px # bibli pour graph
->>>>>>> 71efbba0b1f10bc9ed9afae996aebc5223fcf4ec
 
 local = True # pour avoir un visuel streamlit sur son pc
 
 if local:
-<<<<<<< HEAD
     os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/home/aureliegont/projects/eaupotable-442812-b1a5289718c4.json"
-=======
-    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/home/agathedubly/projects/eaupotable-442812-b1a5289718c4.json"
->>>>>>> 71efbba0b1f10bc9ed9afae996aebc5223fcf4ec
     client = bigquery.Client()
 else:
     credentials = st.secrets["bigquery"]
     client = bigquery.Client.from_service_account_info(credentials)
 
 
-<<<<<<< HEAD
 ### query to select data and turn it into dataframe
 query = "SELECT * FROM eaupotable-442812.dbt_agonternew.mart_ag_evol_prix_qualite_sans_cluster"
-=======
-#Ajouter un titre
-st.title("Comment se structure la gestion de l'eau en France ?")
-
-""
-
-st.subheader("Les services d'eau se concentrent et se professionnalisent")
-
-query = "SELECT * FROM `eaupotable-442812.dbt_adubly.mart_ad_histo_categ_services`"
->>>>>>> 71efbba0b1f10bc9ed9afae996aebc5223fcf4ec
 query_job = client.query(query)
 results = query_job.result()
 columns = [field.name for field in results.schema]
@@ -46,8 +23,6 @@ data = [dict(row.items()) for row in results]
 df = pd.DataFrame(data, columns=columns)
 
 
-
-<<<<<<< HEAD
 
 st.header("La gestion administrative serait-elle un axe d'influence sur le prix de l'eau' ?")
 
@@ -121,4 +96,3 @@ with col2:
     "Population desservie par service categ 1, categ 2, categ 3"
 with col3:
     "A réfléchir"
->>>>>>> 71efbba0b1f10bc9ed9afae996aebc5223fcf4ec
